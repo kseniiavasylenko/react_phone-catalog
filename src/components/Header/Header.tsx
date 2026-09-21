80f23b087fe69dbe2b5eae0907ee79811b16e2e3
Kseniia Myronenko <kseniia.vasylenko11@gmail.com>
2026-09-21 18:06:24 +0300

    Solution

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -20,7 +20,7 @@ export const Header: React.FC = () => {
     <header className={styles.header}>
       <div className={styles.leftContent}>
         <Link to="/" className={styles.logo}>
-          <img src={`${BASE}/img/logo.svg`} alt="Nice Gadgets Logo" />
+          <img src={`${BASE}img/logo.svg`} alt="Nice Gadgets Logo" />
         </Link>
 
         <nav className={styles.nav}>
@@ -41,14 +41,14 @@ export const Header: React.FC = () => {
 
       <div className={styles.rightContent}>
         <NavLink to="/favorites" className={styles.iconBtn}>
-          <img src={`${BASE}/img/icons/favourites.svg`} alt="Favorites" />
+          <img src={`${BASE}img/icons/favourites.svg`} alt="Favorites" />
           {favorites.length > 0 && (
             <span className={styles.badge}>{favorites.length}</span>
           )}
         </NavLink>
 
         <NavLink to="/cart" className={styles.iconBtn}>
-          <img src={`${BASE}/img/icons/shopping-bag.svg`} alt="Cart" />
+          <img src={`${BASE}img/icons/shopping-bag.svg`} alt="Cart" />
           {cartItemsCount > 0 && (
             <span className={styles.badge}>{cartItemsCount}</span>
           )}
