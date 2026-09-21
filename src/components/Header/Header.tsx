86f346150a4ad9f3355d12c7afdb4d98dcfa5feb
Kseniia Myronenko <kseniia.vasylenko11@gmail.com>
2026-09-21 17:48:03 +0300

    Solution

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -18,7 +18,7 @@ export const Header: React.FC = () => {
     <header className={styles.header}>
       <div className={styles.leftContent}>
         <Link to="/" className={styles.logo}>
-          <img src="img/logo.svg" alt="Nice Gadgets Logo" />
+          <img src="/img/logo.svg" alt="Nice Gadgets Logo" />
         </Link>
 
         <nav className={styles.nav}>
@@ -39,17 +39,14 @@ export const Header: React.FC = () => {
 
       <div className={styles.rightContent}>
         <NavLink to="/favorites" className={styles.iconBtn}>
-          <img src="img/icons/favourites.svg" alt="Favorites" />
+          <img src="/img/icons/favourites.svg" alt="Favorites" />
           {favorites.length > 0 && (
             <span className={styles.badge}>{favorites.length}</span>
           )}
         </NavLink>
 
         <NavLink to="/cart" className={styles.iconBtn}>
-          <img
-            src="{`{import.meta.env.BASE_URL}img/icons/shopping-bag.svg`}"
-            alt="Cart"
-          />
+          <img src="/img/icons/shopping-bag.svg" alt="Cart" />
           {cartItemsCount > 0 && (
             <span className={styles.badge}>{cartItemsCount}</span>
           )}
